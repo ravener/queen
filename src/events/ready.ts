35fb43087b017c95f8c78b48fab0ac480812da82
Raven Ravener <ravener.anime@gmail.com>
2024-04-09 17:46:51 +0500

    fix activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -10,6 +10,8 @@ export class Ready {
     console.log(`Logged in as ${client.user!.tag} (${client.user!.id}) in ${client.guilds.cache.size} servers.`);
 
     // Set an activity.
+    client.user!.setActivity({ name: 'Quaver' });
+    // Prevent it from getting reset.
     setInterval(() => client.user!.setActivity({ name: 'Quaver' }), 60 * 60 * 1000);
   }
 }
